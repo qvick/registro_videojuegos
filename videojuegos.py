9713116b36c2fa704d083f1515fc769fd298ab48
qvick <dieg.nunezguocuc.cl>
2025-06-06 10:25:38 -0400

    Paso 2: Registro de videojuego

--- videojuegos.py
+++ videojuegos.py
@@ -13,7 +13,28 @@ while True:
     opcion = input("Seleccione una opcion (1-5): ")
 
     if opcion == 1:
-        pass
+        codigo = int(input("Ingrese el código del videogo: "))
+        nombre = input("Ingrese el nombre del videojuego: ")
+        genero = input("Ingrese el género del videojuego")
+
+        print("\nPlataformas disponibles:")
+        print("1. PC")
+        print("2. PS5")
+        print("3. Xbox Series X")
+        print("4. Nintendo Switch")
+        plataforma_codigo = int(input("Seleccione el número de la plataforma: "))
+        plataforma = plataformas[plataforma_codigo -1 ]
+
+        videojuego = {
+            "codigo": codigo,
+            "nombre": nombre,
+            "genero": genero,
+            "plataforma": plataforma
+        }
+        
+        videojuegos.append(videojuego)
+        print("Videojuego registrado correctamente")
+        
     elif opcion == 2:
         pass
     elif opcion == 3:
