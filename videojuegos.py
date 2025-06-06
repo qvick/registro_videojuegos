fddaf96bd948c05b466c1ef5648885c215c93050
Diego <dieg.nunezguocuc.cl>
2025-06-06 10:18:25 -0400

    Paso 1: Estructura del manú y tupla de plataformas creada

---
+++ videojuegos.py
@@ -0,0 +1,27 @@
+videojuegos = []
+
+plataformas = ("PC", "PS5", "Xbox Series X", "Nintendo Switch")
+
+while True:
+    print("\n--- MENÚ DE VIDEO JUEGOS ---")
+    print("1. Regsitrar Video Juego")
+    print("2. Ver Video Juego")
+    print("3. Modificar Video Juego")
+    print("4. Eliminar Video Juego")
+    print("5. Salir")
+
+    opcion = input("Seleccione una opcion (1-5): ")
+
+    if opcion == 1:
+        pass
+    elif opcion == 2:
+        pass
+    elif opcion == 3:
+        pass
+    elif opcion == 4:
+        pass
+    elif opcion == 5:
+        print("Saliendo del programa.")
+        break
+    else:
+        print("Opcion Inválida. ")
